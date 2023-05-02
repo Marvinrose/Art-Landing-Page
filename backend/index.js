4040eb8fed79deee1d6a5359c6207b8d79a4450e
Chibuezem Marvinrose <rozzeymarvin32@gmail.com>
2023-05-02 13:34:39 +0100

    Installed bootstrap in frontend folder

---
+++ backend/index.js
@@ -0,0 +1,90 @@
+const express = require("express");
+
+const cors = require("cors");
+
+const bcrypt = require("bcryptjs");
+
+const mongoose = require("mongoose");
+
+const jwt = require("jsonwebtoken");
+
+const JWT_SECRET = "jndhohd7yr73tr792yhdhoiqjwhfoi9012y3r9fhgjsik(@uug{58";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const mongoUrl =
+  "mongodb+srv://Art:art@cluster0.vsl3agh.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose
+  .connect(mongoUrl, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("connected to database");
+  })
+  .catch((e) => console.log(e));
+
+require("./userDetails");
+
+const User = mongoose.model("UserInfo");
+
+app.post("/register", async (req, res) => {
+  const { fname, lname, email, password } = req.body;
+
+  const encryptedPassword = await bcrypt.hash(password, 10);
+
+  try {
+    const oldUser = await User.findOne({ email });
+    if (oldUser) {
+      return res.json({ error: "user already exists" });
+    }
+    await User.create({
+      fname,
+      lname,
+      email,
+      password: encryptedPassword,
+    });
+    res.send({ status: "ok" });
+  } catch (error) {
+    res.send({ status: "error" });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.json({ error: "User not found" });
+  }
+  if (await bcrypt.compare(password, user.password)) {
+    const token = jwt.sign({ email: user.email }, JWT_SECRET);
+    if (res.status(201)) {
+      return res.json({ status: "ok", data: token });
+    } else {
+      return res.json({ error: "error" });
+    }
+  }
+  res.json({ status: "error", error: "invalid password" });
+});
+
+app.post("/home", async (req, res) => {
+  const { token } = req.body;
+  try {
+    const user = jwt.verify(token, JWT_SECRET);
+    const useremail = user.email;
+    User.findOne({ email: useremail })
+      .then((data) => {
+        res.send({ status: "ok", data: data });
+      })
+      .catch((error) => {
+        res.send({ status: "error", data: error });
+      });
+  } catch (error) {}
+});
+
+const port = 9000;
+
+app.listen(port, () => console.log(`example app listening on port ${port}`));
