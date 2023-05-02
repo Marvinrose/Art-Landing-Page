7bf15a19bc8f46e6910c050d188c2b856b5f1aee
Chibuezem Marvinrose <rozzeymarvin32@gmail.com>
2023-05-02 13:22:45 +0100

    Created a new image folder and added images

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,120 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "../App.css";
+
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const { email, password } = this.state;
+    console.log(email, password);
+    fetch("http://localhost:9000/login", {
+      method: "POST",
+      crossDomain: true,
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data, "userRegister");
+        if (data.status === "ok") {
+          alert("Login Successful!");
+          window.localStorage.setItem("token", data.data);
+          window.location.href = "./Home";
+        } else if (data.error === "invalid password") {
+          alert("invalid password");
+        }
+        if (data.error === "User not found") {
+          alert("User not found");
+        }
+      });
+  }
+
+  render() {
+    return (
+      <div
+        className=" App p-5 mb-5
+    "
+      >
+        <h1 className="text-center">Login</h1>
+        <form
+          className="form-horizontal"
+          action=""
+          onSubmit={this.handleSubmit}
+        >
+          <div className="form-group">
+            <label for="email">Email</label>
+            <input
+              type="text"
+              className="form-control"
+              id="email"
+              placeholder="Enter email address"
+              onChange={(e) => this.setState({ email: e.target.value })}
+            ></input>
+          </div>
+
+          <div className="form-group pt-3">
+            <label for="userpassword">Password</label>
+            <input
+              type="password"
+              class="form-control"
+              id="userpassword"
+              placeholder="Enter password"
+              onChange={(e) => this.setState({ password: e.target.value })}
+            ></input>
+          </div>
+
+          <div className="custom-control custom-checkbox">
+            <input
+              type="checkbox"
+              class="custom-control-input"
+              id="customControlInline"
+            ></input>
+            <label
+              className="custom-control-label pt-2"
+              for="customControlInline"
+            >
+              Remember me
+            </label>
+          </div>
+
+          <div className="mt-3 text-center">
+            <button
+              className="btn btn-primary btn-block waves-effect waves-light"
+              type="submit"
+            >
+              Log In
+            </button>
+          </div>
+
+          <div className="mt-4 text-center">
+            <a href="/" class="text-muted">
+              <i class="mdi mdi-lock mr-1"></i> Forgot your password?
+            </a>
+          </div>
+
+          <div className="mt-4 ">
+            <p className="text-end">
+              Don't have an account? <Link to="/Register"> Sign Up</Link>
+            </p>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
---
+++ frontend/src/components/Register.js
@@ -0,0 +1,119 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "../App.css";
+
+export default class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fname: "",
+      lname: "",
+      email: "",
+      password: "",
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    const { fname, lname, email, password } = this.state;
+    console.log(fname, lname, email, password);
+    fetch("http://localhost:9000/register", {
+      method: "POST",
+      crossDomain: true,
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({
+        fname,
+        lname,
+        email,
+        password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data, "userRegister");
+        if (data.error === "user already exists") {
+          alert("User already exists, try logging in");
+          window.localStorage.getItem("token", data.data);
+        }
+
+        if (data.status === "ok") {
+          alert("Successfully Registered, you can now Sign in!");
+        }
+      });
+  }
+  render() {
+    return (
+      <div className="App p-5 mb-5 ">
+        <h1 className="text-center">Register</h1>
+        <form
+          className="form-horizontal"
+          action=""
+          onSubmit={this.handleSubmit}
+        >
+          <div className="form-group">
+            <label for="username">Firstname</label>
+            <input
+              type="text"
+              className="form-control"
+              id="firstname"
+              placeholder="Firstname"
+              onChange={(e) => this.setState({ fname: e.target.value })}
+            ></input>
+          </div>
+
+          <div className="form-group pt-3">
+            <label for="userpassword">Lastname</label>
+            <input
+              type="text"
+              class="form-control"
+              id="lastname"
+              placeholder="Lastname"
+              onChange={(e) => this.setState({ lname: e.target.value })}
+            ></input>
+          </div>
+
+          <div className="form-group pt-3">
+            <label for="userpassword">Email Address</label>
+            <input
+              type="email"
+              class="form-control"
+              id="email"
+              placeholder=""
+              onChange={(e) => this.setState({ email: e.target.value })}
+            ></input>
+          </div>
+
+          <div className="form-group pt-3">
+            <label for="userpassword">Password</label>
+            <input
+              type="text"
+              class="form-control"
+              id="password"
+              placeholder="Enter password"
+              onChange={(e) => this.setState({ password: e.target.value })}
+            ></input>
+          </div>
+
+          <div className="mt-3 text-center">
+            <button
+              className="btn btn-primary btn-block waves-effect waves-light"
+              type="submit"
+            >
+              Register
+            </button>
+          </div>
+
+          <div className="mt-4 ">
+            <p className="text-end">
+              Already registered? <Link to="/">Sign In</Link>
+            </p>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
