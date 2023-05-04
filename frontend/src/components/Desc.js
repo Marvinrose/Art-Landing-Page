6360f6a57ce0cb066bf97e9c15c5475cfad83990
Chibuezem Marvinrose <rozzeymarvin32@gmail.com>
2023-05-04 16:20:04 +0100

    Edited desc.js

--- frontend/src/components/Desc.js
+++ frontend/src/components/Desc.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+// import axios from "axios";
 import desc from "../images/desc-img.png";
 import { Link } from "react-router-dom";
 import Navbar from "./Navbar";
@@ -7,23 +7,23 @@ import Navbar from "./Navbar";
 import "../desc.css";
 
 export default function Desc() {
-  const [art, setArt] = useState([]);
-  let apiKey = 129884;
+//   const [art, setArt] = useState([]);
+//   let apiKey = 129884;
 
-  let apiUrl = `https://api.artic.edu/api/v1/artworks?=${apiKey}fields=id,title,artist_display,date_display,main_reference_number`;
+//   let apiUrl = `https://api.artic.edu/api/v1/artworks?=${apiKey}fields=id,title,artist_display,date_display,main_reference_number`;
 
-  useEffect(() => {
-    axios
-      .get(apiUrl)
-      .then((res) => {
-        console.log(res.data.data);
-        console.log(res.data.data[4].exhibition_history);
-        setArt(res.data.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, [apiUrl]);
+//   useEffect(() => {
+//     axios
+//       .get(apiUrl)
+//       .then((res) => {
+//         console.log(res.data.data);
+//         console.log(res.data.data[4].exhibition_history);
+//         setArt(res.data.data);
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       });
+//   }, [apiUrl]);
 
   <>
     return (
@@ -55,14 +55,14 @@ export default function Desc() {
             <div className="">
               <h3 className="mb-3">Plastic Hears Sculpture</h3>
               <p>
-                {art[0].artist_display}
-                {/* Browse a curated selection of art around the world, including
+                {/* {art[0].artist_display} */}
+                Browse a curated selection of art around the world, including
                 museum exhibitions, gallery openings, upcoming and many more.
                 Browse a curated selection of art around the world, including
                 museum exhibitions, gallery openings, upcoming and many
                 moreBrowse a curated selection of art around the world,
                 including museum exhibitions, gallery openings, upcoming and
-                many more */}
+                many more
               </p>
             </div>
           </div>
