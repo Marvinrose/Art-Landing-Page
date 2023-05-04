b76f506ee758140897ba054d57ebf3830065893a
Chibuezem Marvinrose <rozzeymarvin32@gmail.com>
2023-05-04 13:29:16 +0100

    Edited Desc.js

--- frontend/src/components/Desc.js
+++ frontend/src/components/Desc.js
@@ -10,39 +10,45 @@ export default class Desc extends Component {
     return (
       <div className="">
         <Navbar />
-        <div className="row container-fluid">
-          <div className="col-6 text-left">
-            <Link to="/Hero">
+        <div>
+          <div className="row container-fluid mt-5">
+            <div className="col-6 text-left">
+              <Link to="/Hero" className="link-desc">
+                <h2>
+                  {" "}
+                  <i class="fa-solid fa-arrow-left"></i>
+                </h2>
+              </Link>
+            </div>
+            <div className="col-6 text-right">
               <h2>
-                {" "}
-                <i class="fa-solid fa-arrow-left"></i>
+                <i class="fa-sharp fa-regular fa-share-nodes"></i>
               </h2>
-            </Link>
-          </div>
-          <div className="col-6 text-right">
-            <h2>
-              <i class="fa-sharp fa-regular fa-share-nodes"></i>
-            </h2>
-          </div>
-        </div>
-        <div className="container row">
-          <div className="col-6">
-            <div className="art-blob">
-              <img src={desc} className="img-fluid " alt="artimage"></img>
             </div>
           </div>
-          <div className="col-6">
-            <div className="">
-              <h3 className="mb-3">Plastic Hears Sculpture</h3>
-              <p>
-                Browse a curated selection of art around the world, including
-                museum exhibitions, gallery openings, upcoming and many more.
-                Browse a curated selection of art around the world, including
-                museum exhibitions, gallery openings, upcoming and many
-                moreBrowse a curated selection of art around the world,
-                including museum exhibitions, gallery openings, upcoming and
-                many more
-              </p>
+          <div className="container row">
+            <div className="col-6">
+              <div className="art-blob">
+                <img
+                  src={desc}
+                  className="img-fluid art-blob"
+                  alt="artimage"
+                ></img>
+              </div>
+            </div>
+            <div className="col-6">
+              <div className="">
+                <h3 className="mb-3">Plastic Hears Sculpture</h3>
+                <p>
+                  Browse a curated selection of art around the world, including
+                  museum exhibitions, gallery openings, upcoming and many more.
+                  Browse a curated selection of art around the world, including
+                  museum exhibitions, gallery openings, upcoming and many
+                  moreBrowse a curated selection of art around the world,
+                  including museum exhibitions, gallery openings, upcoming and
+                  many more
+                </p>
+              </div>
             </div>
           </div>
         </div>
