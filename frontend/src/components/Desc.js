56e2e4139adc65c94d8358af8b518f04b3153c5b
Chibuezem Marvinrose <rozzeymarvin32@gmail.com>
2023-05-04 16:25:00 +0100

    Updated desc.js

--- frontend/src/components/Desc.js
+++ frontend/src/components/Desc.js
@@ -7,26 +7,7 @@ import Navbar from "./Navbar";
 import "../desc.css";
 
 export default function Desc() {
-  //   const [art, setArt] = useState([]);
-  //   let apiKey = 129884;
-
-  //   let apiUrl = `https://api.artic.edu/api/v1/artworks?=${apiKey}fields=id,title,artist_display,date_display,main_reference_number`;
-
-  //   useEffect(() => {
-  //     axios
-  //       .get(apiUrl)
-  //       .then((res) => {
-  //         console.log(res.data.data);
-  //         console.log(res.data.data[4].exhibition_history);
-  //         setArt(res.data.data);
-  //       })
-  //       .catch((err) => {
-  //         console.log(err);
-  //       });
-  //   }, [apiUrl]);
-
-  <>
-    return (
+  return (
     <div className="">
       <Navbar />
       <div>
@@ -55,7 +36,6 @@ export default function Desc() {
             <div className="">
               <h3 className="mb-3">Plastic Hears Sculpture</h3>
               <p>
-                {/* {art[0].artist_display} */}
                 Browse a curated selection of art around the world, including
                 museum exhibitions, gallery openings, upcoming and many more.
                 Browse a curated selection of art around the world, including
@@ -69,6 +49,5 @@ export default function Desc() {
         </div>
       </div>
     </div>
-    );
-  </>;
+  );
 }
