2a4af88ee048f47e1e27157c496e204054b0a867
Chibuezem Marvinrose <rozzeymarvin32@gmail.com>
2023-05-04 17:08:27 +0100

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-// import "../App.css";
+import "../App.css";
 import "../login.css";
 
 export default class Login extends Component {
@@ -49,7 +49,7 @@ export default class Login extends Component {
   render() {
     return (
       <div
-        className="login p-5 mb-5
+        className="login  p-5 mb-5
     "
       >
         <h1 className="text-center">Login</h1>
