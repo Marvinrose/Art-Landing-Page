2b18111b5e2aa4b546c33c6f0648712be3987617
Chibuezem Marvinrose <rozzeymarvin32@gmail.com>
2023-05-04 13:47:51 +0100

    Added navbar component to hero.js

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -1,5 +1,5 @@
 import React from "react";
-import icon from "../images/icon.png";
+import Navbar from "./Navbar";
 
 import "../hero.css";
 import image from "../images/hero-image.png";
@@ -7,82 +7,7 @@ import image from "../images/hero-image.png";
 export default function Hero() {
   return (
     <div className="hero container-fluid  ">
-      <div className="">
-        <nav class="navbar navbar-expand-lg fixed-top">
-          <div class="container-fluid">
-            <a
-              class="navbar-brand fw-bold text-white"
-              href="9"
-              rel="noreferrer"
-            >
-              <img
-                className="img-fluid align-img"
-                width={25}
-                src={icon}
-                alt="icon"
-              ></img>
-              Artisto
-            </a>
-            <button
-              class="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNavAltMarkup"
-              aria-controls="navbarNavAltMarkup"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-              <div class="navbar-nav ml-auto">
-                <a
-                  class="nav-link active text-white fw-bold"
-                  aria-current="page"
-                  href="9"
-                >
-                  Events
-                </a>
-                <a
-                  class="nav-link text-white fw-bold"
-                  href="9"
-                  rel="noreferrer"
-                >
-                  Museum
-                </a>
-                <a
-                  class="nav-link text-white fw-bold"
-                  href="9"
-                  rel="noreferrer"
-                >
-                  Arts
-                </a>
-                <a
-                  class="nav-link text-white fw-bold"
-                  href="9"
-                  rel="noreferrer"
-                >
-                  Gallaries
-                </a>
-                <a
-                  class="nav-link login fw-bold text-dark"
-                  href="9"
-                  rel="noreferrer"
-                >
-                  Login
-                </a>
-                <a
-                  class="nav-link explore fw-bold text-dark"
-                  href="9"
-                  rel="noreferrer"
-                >
-                  Explore arts
-                </a>
-              </div>
-            </div>
-          </div>
-        </nav>
-      </div>
+      <Navbar />
 
       <div className="container-fluid">
         <div className="row pt-5 mb-5  d-none d-md-flex hero-section">
