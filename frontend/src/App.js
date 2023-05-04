b9880b94818b4ae2adf4474afbe89cac0d8caf04
Chibuezem Marvinrose <rozzeymarvin32@gmail.com>
2023-05-04 13:45:49 +0100

    Updated the routes in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Login from "./components/Login";
 import Register from "./components/Register";
 import Home from "./components/Home";
 import Desc from "./components/Desc";
+import Hero from "./components/Hero";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="Register" element={<Register />} />
         <Route path="Home" element={<Home />} />
         <Route path="Desc" element={<Desc />} />
+        <Route path="Hero" element={<Hero />} />
       </Routes>
     </div>
   );
