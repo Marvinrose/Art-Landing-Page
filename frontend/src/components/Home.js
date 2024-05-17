53f9a95d0f96efe09d6219f96b83d24e0a72b503
Chibuezem Marvinrose <rozzeymarvin32@gmail.com>
2024-05-17 16:50:32 +0100

    Updated

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -28,7 +28,7 @@ export default class Home extends Component {
 
   render() {
     return (
-      <div>
+      <div className="container-fluid">
         <Hero />
         <Filter />
         <ArtSection defaultKeyword="mask" />
