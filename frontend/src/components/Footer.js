8782ff0d904a6d9a250fe95b2ed784bb8fdb403b
Chibuezem Marvinrose <rozzeymarvin32@gmail.com>
2024-05-30 17:32:09 +0100

    Updated footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,6 +6,7 @@ const Footer = () => {
     <div className="text-center pb-3" style={{ fontSize: "14px" }}>
       Built with ❤️ by{" "}
       <Link
+        target="_blank"
         style={{ textDecoration: "none", color: "var(--secondary-color)" }}
         to="https://react-portfolio-delta-nine.vercel.app/"
       >
